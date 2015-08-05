59c68f84da5c62934b4caba6883e4f0b1c50bafc
Dr-Zer0 <Dr-Zer0@suremail.info>
2015-08-05 23:02:21 +0200

    added altadefinizione01.com

---
+++ altadefinizione01.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# pelisalacarta - XBMC Plugin
+# Canal para piratestreaming
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+# ------------------------------------------------------------
+import urlparse, urllib2, urllib, re
+import os, sys
+
+from core import logger
+from core import config
+from core import scrapertools
+from core.item import Item
+from servers import servertools
+
+__channel__ = "altadefinizione01"
+__category__ = "F,S,A"
+__type__ = "generic"
+__title__ = "AltaDefinizione01"
+__language__ = "IT"
+
+sito = "http://www.altadefinizione01.com/"
+
+DEBUG = config.get_setting("debug")
+
+
+def isGeneric():
+    return True
+
+
+def mainlist(item):
+    logger.info("pelisalacarta.altadefinizione01 mainlist")
+
+    itemlist = [Item(channel=__channel__,
+                     title="[COLOR azure]Ultimi film inseriti[/COLOR]",
+                     action="peliculas",
+                     url=sito,
+                     thumbnail="http://dc584.4shared.com/img/XImgcB94/s7/13feaf0b538/saquinho_de_pipoca_01"),
+                Item(channel=__channel__,
+                     title="[COLOR azure]Categorie film[/COLOR]",
+                     action="categorias",
+                     url=sito,
+                     thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"),
+                Item(channel=__channel__,
+                     title="[COLOR yellow]Cerca...[/COLOR]",
+                     action="search",
+                     thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")]
+
+    return itemlist
+
+
+def peliculas(item):
+    logger.info("pelisalacarta.altadefinizione01 peliculas")
+    itemlist = []
+
+    # Descarga la pagina
+    data = scrapertools.cache_page(item.url)
+
+    # Extrae las entradas (carpetas)
+    patron = '<a\s+href="([^"]+)"\s+title="[^"]*">\s+<img\s+width="[^"]*"\s+height="[^"]*"\s+src="([^"]+)"\s+class="[^"]*"\s+alt="([^"]+)"'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+    scrapertools.printMatches(matches)
+
+    #for scrapedurl, scrapedtitle, scrapedthumbnail in matches:
+    for scrapedurl, scrapedthumbnail, scrapedtitle in matches:
+        html = scrapertools.cache_page(scrapedurl)
+        start = html.find("<div class=\"aciklama\">")
+        end = html.find(">Fonte Trama</a>", start)
+        scrapedplot = html[start:end]
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle.replace("Streaming", ""))
+        if scrapedplot.startswith("<div class"):
+            scrapedplot = scrapedplot[30:]
+        if DEBUG: logger.info("title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="play",
+                 title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                 url=scrapedurl,
+                 viewmode="movie_with_plot",
+                 thumbnail=scrapedthumbnail,
+                 plot=scrapedplot,
+                 folder=True))
+
+    # Extrae el paginador
+    patronvideos = 'class="nextpostslink" rel="next" href="([^"]+)">&raquo;'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
+    scrapertools.printMatches(matches)
+
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="peliculas",
+                 title="[COLOR orange]Successivo >>[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                 folder=True))
+
+    return itemlist
+
+
+def categorias(item):
+    logger.info("pelisalacarta.altadefinizione01 categorias")
+    itemlist = []
+
+    data = scrapertools.cache_page(item.url)
+    logger.info(data)
+
+    # Narrow search by selecting only the combo
+    bloque = scrapertools.get_match(data, '<ul class="kategori_list">(.*?)</ul>')
+
+    # The categories are the options for the combo  
+    patron = '<li><a href="([^"]+)">([^<]+)</a></li>'
+    matches = re.compile(patron, re.DOTALL).findall(bloque)
+    scrapertools.printMatches(matches)
+
+    for url, titulo in matches:
+        scrapedtitle = titulo
+        scrapedurl = urlparse.urljoin(item.url, url)
+        scrapedthumbnail = ""
+        scrapedplot = ""
+        if DEBUG: logger.info("title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="peliculas",
+                 title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail=scrapedthumbnail,
+                 plot=scrapedplot))
+
+    return itemlist
+
+
+def search(item, texto):
+    logger.info("[altadefinizione01.py] " + item.url + " search " + texto)
+    item.url = "%sindex.php/?s=%s" % (sito, texto)
+    try:
+        return peliculas(item)
+    # Se captura la excepción, para no interrumpir al buscador global si un canal falla
+    except:
+        import sys
+        for line in sys.exc_info():
+            logger.error("%s" % line)
+        return []
+
+
+def play(item):
+    logger.info("[altadefinizione01.py] play")
+    itemlist = []
+    video_url = None
+
+    data = scrapertools.cache_page(item.url)
+    url = scrapertools.find_single_match(data, '<IFRAME SRC="([^"]+)"')
+    if url != "":
+        data = scrapertools.cache_page(url)
+        data = scrapertools.find_single_match(data, "(eval.function.p,a,c,k,e,.*?)\s*</script>")
+        if data != "":
+            from lib.jsbeautifier.unpackers import packer
+            data = packer.unpack(data)
+            video_url = scrapertools.find_single_match(data, 'file\s*:\s*"([^"]+)",')
+
+    if video_url is not None:
+        item.url = video_url
+        item.server = None
+        itemlist.append(item)
+
+    return itemlist
