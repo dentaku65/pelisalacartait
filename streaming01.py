d100fcdcfcbde81eb4366019d88c9e5194914827
Dentaku65 <dentaku65@gmail.com>
2015-08-23 15:03:31 +0200

    new channel, with plot

---
+++ streaming01.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# pelisalacarta - XBMC Plugin
+# Canal para piratestreaming
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+#------------------------------------------------------------
+import urlparse,urllib2,urllib,re
+import os, sys
+
+from core import logger
+from core import config
+from core import scrapertools
+from core.item import Item
+from servers import servertools
+
+__channel__ = "streaming01"
+__category__ = "F"
+__type__ = "generic"
+__title__ = "Streaming01.com (IT)"
+__language__ = "IT"
+
+DEBUG = config.get_setting("debug")
+
+def isGeneric():
+    return True
+
+def mainlist(item):
+    logger.info("pelisalacarta.streaming01 mainlist")
+    itemlist = []
+    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Ultimi Film Inseriti[/COLOR]", action="peliculas", url="http://www.streaming01.com/", thumbnail="http://dc584.4shared.com/img/XImgcB94/s7/13feaf0b538/saquinho_de_pipoca_01"))
+    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Film Per Categoria[/COLOR]", action="categorias", url="http://www.streaming01.com/", thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"))
+    itemlist.append( Item(channel=__channel__, title="[COLOR yellow]Cerca...[/COLOR]", action="search", thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search"))
+
+    
+    return itemlist
+
+def categorias(item):
+    itemlist = []
+    
+    # Descarga la pagina
+    data = scrapertools.cache_page(item.url)
+    bloque = scrapertools.get_match(data,'<ul class="main-menu clearfix">(.*?)</ul>')
+    
+    # Extrae las entradas (carpetas)
+    patron  = '<li><a href="(.*?)">(.*?)</a></li>'
+    matches = re.compile(patron,re.DOTALL).findall(bloque)
+    scrapertools.printMatches(matches)
+
+    for scrapedurl,scrapedtitle in matches:
+        scrapedplot = ""
+        scrapedthumbnail = ""
+        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"]")
+        itemlist.append( Item(channel=__channel__, action="peliculas", title="[COLOR azure]"+scrapedtitle+"[/COLOR]" , url=scrapedurl , thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png", folder=True) )
+
+    return itemlist
+
+def search(item,texto):
+    logger.info("[streaming01.py] "+item.url+" search "+texto)
+    item.url = "http://www.streaming01.com/index.php?do=search&subaction=search&story="+texto
+    try:
+        return peliculas(item)
+    # Se captura la excepción, para no interrumpir al buscador global si un canal falla
+    except:
+        import sys
+        for line in sys.exc_info():
+            logger.error( "%s" % line )
+        return []
+
+def peliculas(item):
+    logger.info("pelisalacarta.streaming01 peliculas")
+    itemlist = []
+
+    # Descarga la pagina
+    data = scrapertools.cache_page(item.url)
+
+    # Extrae las entradas (carpetas)
+    patron = '<a class="short-img" href="(.*?)"[^>]+>\s*'
+    patron += '<img src="(.*?)"[^>]+>\s*'
+    patron += '</a>\s*'
+    patron += '<div[^>]+>\s*'
+    patron += '<h3>[^>]+>(.*?)</a></h3>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    scrapertools.printMatches(matches)
+
+    for scrapedurl,scrapedthumbnail,scrapedtitle in matches:
+        response = urllib2.urlopen(scrapedurl)
+        html = response.read()
+        start = html.find("<div class=\"full-text clearfix desc-text\">")
+        end = html.find("<table>", start)
+        scrapedplot = html[start:end]
+        scrapedplot = re.sub(r'<.*?>', '', scrapedplot)
+        scrapedplot = scrapertools.decodeHtmlentities(scrapedplot)
+        #scrapedplot = ""
+        scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("Streaming download gratis ",""))
+        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+        itemlist.append( Item(channel=__channel__, action="findvideos", title="[COLOR azure]"+scrapedtitle+"[/COLOR]" , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
+
+    # Extrae el paginador
+    patronvideos  = '<span class="pnext"><a href="(.*?)">Avanti</a></span>'
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+    scrapertools.printMatches(matches)
+
+    if len(matches)>0:
+        scrapedurl = urlparse.urljoin(item.url,matches[0])
+        itemlist.append( Item(channel=__channel__, action="peliculas", title="[COLOR orange]Successivo>>[/COLOR]" , url=scrapedurl , thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png", folder=True) )
+
+    return itemlist
+
+def test():
+    from servers import servertools
+    
+    # mainlist
+    mainlist_items = mainlist(Item())
+    # Da por bueno el canal si alguno de los videos de "Novedades" devuelve mirrors
+    novedades_items = peliculas(mainlist_items[0])
+    bien = False
+    for novedades_item in novedades_items:
+        mirrors = servertools.find_video_items( item=novedades_item )
+        if len(mirrors)>0:
+            bien = True
+            break
+
+    return bien
