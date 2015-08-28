af94b594eb43800c7ea82745dfe9f34a1115395a
Dentaku65 <dentaku65@gmail.com>
2015-08-28 19:41:23 +0200

    small fix

--- tantifilm.py
+++ tantifilm.py
@@ -23,12 +23,22 @@ DEBUG = config.get_setting("debug")
 
 sito="http://www.tantifilm.net"
 
+headers = [
+    ['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0'],
+    ['Accept-Encoding', 'gzip, deflate'],
+    ['Referer', 'http://www.tantifilm.net/'],
+    ['Connection', 'keep-alive']
+]
+
+
+
 def isGeneric():
     return True
 
 def mainlist(item):
     logger.info("pelisalacarta.tantifilm mainlist")
     itemlist = []
+    itemlist.append( Item(channel=__channel__, title="[COLOR azure]Ultimi film[/COLOR]", action="latest", url="http://www.tantifilm.net/", thumbnail="http://dc584.4shared.com/img/XImgcB94/s7/13feaf0b538/saquinho_de_pipoca_01"))
     itemlist.append( Item(channel=__channel__, title="[COLOR azure]Al Cinema[/COLOR]", action="peliculas", url="http://www.tantifilm.net/watch-genre/al-cinema/", thumbnail="http://dc584.4shared.com/img/XImgcB94/s7/13feaf0b538/saquinho_de_pipoca_01"))
     itemlist.append( Item(channel=__channel__, title="[COLOR azure]HD - Alta Definizione[/COLOR]", action="peliculas", url="http://www.tantifilm.net/watch-genre/hd-alta-qualita/", thumbnail="http://dc584.4shared.com/img/XImgcB94/s7/13feaf0b538/saquinho_de_pipoca_01"))
     itemlist.append( Item(channel=__channel__, title="[COLOR azure]Film Per Categoria[/COLOR]", action="categorias", url="http://www.tantifilm.net/", thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/All%20Movies%20by%20Genre.png"))
@@ -74,7 +84,7 @@ def peliculas(item):
     itemlist = []
 
     # Descarga la pagina
-    data = scrapertools.cache_page(item.url)
+    data = scrapertools.cache_page(item.url, headers=headers)
 
     # Extrae las entradas (carpetas)
     patron = '<div class="media3">[^>]+><a href="(.*?)"><img[^s]+src="(.*?)"[^>]+></a><[^>]+><a[^>]+><p>(.*?)</p></a></div>'
@@ -90,6 +100,46 @@ def peliculas(item):
         scrapedplot = re.sub(r'<[^>]*>', '', scrapedplot)
         scrapedplot = scrapertools.decodeHtmlentities(scrapedplot)
         scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
+        scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("streaming",""))
+        #scrapedplot = ""
+        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+        itemlist.append( Item(channel=__channel__, action="findvideos", title="[COLOR azure]"+scrapedtitle+"[/COLOR]" , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
+
+    # Extrae el paginador
+    patronvideos  = '<a class="nextpostslink" rel="next" href="(.*?)">»</a>'
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+    scrapertools.printMatches(matches)
+
+    if len(matches)>0:
+        scrapedurl = urlparse.urljoin(item.url,matches[0])
+        itemlist.append( Item(channel=__channel__, action="peliculas", title="[COLOR orange]Successivo>>[/COLOR]" , url=scrapedurl , thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png", folder=True) )
+
+    return itemlist
+
+def latest(item):
+    logger.info("pelisalacarta.tantifilm peliculas")
+    itemlist = []
+
+    # Descarga la pagina
+    data = scrapertools.cache_page(item.url)
+
+    # Extrae las entradas (carpetas)
+    patron = '<div class="mediaWrap mediaWrapAlt">\s*'
+    patron += '<a href="(.*?)" title="(.*?)" rel="bookmark">\s*'
+    patron += '<img[^s]+src="(.*?)"[^>]+>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    scrapertools.printMatches(matches)
+
+    for scrapedurl,scrapedtitle,scrapedthumbnail in matches:
+        response = urllib2.urlopen(scrapedurl)
+        html = response.read()
+        start = html.find("<div class=\"content-left-film\">")
+        end = html.find("</div>", start)
+        scrapedplot = html[start:end]
+        scrapedplot = re.sub(r'<[^>]*>', '', scrapedplot)
+        scrapedplot = scrapertools.decodeHtmlentities(scrapedplot)
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
+        scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("Permalink to ",""))
         scrapedtitle=scrapertools.decodeHtmlentities(scrapedtitle.replace("streaming",""))
         #scrapedplot = ""
         if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
